0581044c48a4f5d54e188f50520a0fed2eff31ab
Felipe <abelardo.felipe@galileo.edu>
2021-02-15 19:28:56 -0600

    Proyecto_Final

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Proyecto-Final
+
+Al iniciar el programa se le pregunta el nombre al asegurado, se le da un espacio para contestar. Luego se le realizan una serie de preguntas como su edad, si está casado, en caso de que este casado se le pregunta la edad de su esposo/a, luego se le pregunta si tien hijos o no, en caso de que si tenga se le pregunta cuantos tiene, luego se le pregunta si tiene propiedades, si la respuesta es si se le pregunta cuantas propiedades tiene, por ultimo se le pregunta si trabaja o no, si trabaja se le pregunta cuanto gana al mes.
+
+Luego de la serie de preguntas, el programa empieza a calcular en base a los datos proporcionados por el usuario, calcula uno por uno, lo recargos que se le tiene que aplicar a la cotización, el recargo por edad del asegurado, el recargo por la edad de su esposo/a si tiene esposo/a, l recargo por la cantidad de hijos si tiene hijos, el recargo por la cantidad de propiedades que posee si es que posee propiedadesy el recargo por la cantidad de dinero que gana al mes si es que cuenta con un trabajo.
+
+Todo esto lo va guardando el programa y al final le muestra al asegurado su nombre, la cantidad de dinero de los recargos, el precio final de la cotización.
+
+Por ultimo le muestra al asegurado que escriba la palabra "salir" si quiere salir del programa de lo contrario podrá seguri haciendo mas cotizaciones.
---
+++ proyecto.js
@@ -0,0 +1,134 @@
+//Variables utiles 
+//Precio base de la cotización, en quetzales, lo puede cambiar
+var precio_base = 2000
+
+//Valores de los recargos 
+var edad_18 = 0.1 // 10%
+var edad_25 = 0.2 // 20%
+var edad_50 = 0.3 // 30%
+
+var casado_18 = 0.1 // 10%
+var casado_25 = 0.2 // 20%
+var casado_50 = 0.3 // 30%
+
+var hijo = 0.2 // 20%
+
+var propiedad = 0.35
+
+var sueldos = 0.05
+
+//Recargo
+var recargo = 0
+var recargo_total = 0
+
+//Precio final 
+var precio_final = 0
+
+var salida
+while (salida != "salir") {
+  
+
+
+//Mensajes de alerta para ingresar datos 
+var nombre = prompt("Ingrese su nombre, por favor")
+var edad = prompt("¿Cuantos años tiene? Ingrese solamente números ")
+
+var casado = prompt("¿Está casado actualmente?")
+//Comprobamos la edad del cónyuge, solamente si se está casado/a
+var edad_conyuge
+if("SI" == casado.toUpperCase()){
+  edad_conyuge = prompt("¿Que edad tiene su esposo/a?")
+}
+//convirtiendo las edades ingresadas a números 
+var edad_numero = parseInt(edad)
+var edad_conyuge_numero = 0
+//convirtiendo la edad del cónyuge si se esta casado/a
+if("SI" == casado.toUpperCase()){
+  edad_conyuge_numero = parseInt(edad_conyuge)
+}
+
+var hijos = prompt("¿Tienes hijos o hijas?")
+//Comprobamos la edad del cónyuge, solamente si se está casado/a
+var cantidad_hijos
+if ("SI" == hijos.toUpperCase()) {
+  cantidad_hijos = prompt("¿Cuantos hijos o hijas tiene?")
+}
+var cantidad_hijos_numero = 0
+if ("SI" == hijos.toUpperCase()){
+  cantidad_hijos_numero = parseInt(cantidad_hijos)
+}
+
+var propiedades = prompt("¿Tiene propiedades?")
+var cantidad_propiedades
+if("SI" == propiedades.toUpperCase()){
+  cantidad_propiedades = prompt("¿Cuantas propiedades tiene?")
+}
+var cantidad_propiedades_numero = 0
+if("SI" == propiedades.toUpperCase()){
+  cantidad_propiedades_numero = parseInt(cantidad_propiedades)
+}
+
+var trabaja = prompt("¿Actualmente trabaja?")
+var sueldo
+if("SI" == trabaja.toUpperCase()){
+  sueldo = prompt("¿Cuanto es su salario mensual?")
+}
+var sueldo_numero = 0
+if("SI" == trabaja.toUpperCase()){
+  sueldo_numero = parseInt(sueldo)
+}
+
+//Calculamos el recargo por los rangos de edades
+if(edad_numero>=18 && edad_numero<25){
+  recargo = precio_base * edad_18
+  recargo_total = recargo_total + recargo
+}else if(edad_numero>=25 && edad_numero<50){
+  recargo = precio_base * edad_25
+  recargo_total = recargo_total + recargo
+}else if(edad_numero>=50 && edad_numero<100){
+  recargo = precio_base *edad_50
+  recargo_total = recargo_total + recargo
+}
+
+/** 
+ * 2. Recargo por la edad del conyuge
+ */
+if(edad_conyuge_numero>=18 && edad_conyuge_numero<25){
+  recargo = precio_base * casado_18
+  recargo_total = recargo_total + recargo
+}else if(edad_conyuge_numero>=25 && edad_conyuge_numero<50){
+  recargo = precio_base * casado_25
+  recargo_total = recargo_total + recargo
+}else if(edad_conyuge_numero>=50 && edad_conyuge_numero<100){
+  recargo = precio_base * casado_50
+  recargo_total = recargo_total + recargo
+}
+
+/**
+ * 3. Recargo por la cantidad de hijos 
+ */ 
+if(cantidad_hijos_numero>=1){
+  recargo = precio_base * hijo * cantidad_hijos_numero
+  recargo_total = recargo_total + recargo
+}
+
+//Calculamos el recargo de propiedades
+if(cantidad_propiedades_numero>=1){
+  recargo = precio_base * propiedad * cantidad_propiedades_numero
+  recargo_total = recargo_total + recargo
+}
+
+//Calculamos el recargo por el sueldo
+if(sueldo_numero>=1){
+  recargo = sueldo_numero * sueldos
+  recargo_total = recargo_total + recargo
+}
+
+precio_final = precio_base + recargo_total
+//Resultado
+alert ("Para el asegurado "+nombre)
+alert ("El recargo total sera de: "+recargo_total)
+alert ("El precio sera de: "+precio_final)
+
+salida = prompt('Escriba la palabra "salir" si ya no desea hacer otra consulta')
+}
